8144c2e56525f0cfa5e84232912e1d8241006074
speckij <speckij@posteo.net>
2023-09-27 15:10:39 +0200

    lower amount of initially requested points
    With common measurement intervals of 1minute, the 10minutes max_timespan
    filter removes 90% of the observations anyway.

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,3 +1,5 @@
-export const LAST_MEASUREMENTS_COUNT = 200;
+// normal measuring interval is 1minute
+export const LAST_MEASUREMENTS_COUNT = 12;
 
+// Only show data in window of 10 minutes from last timestamp
 export const MAX_TIMESPAN_IN_MS_FOR_CHART = 600000;
